21006b51f40b96e16b4be59e4564a6505e0df9d6
Anro <anelnichenko@bk.ru>
2025-05-27 01:44:18 +0300

    Задание

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import Header from './components/Header';
+import ArtistsPage from './pages/ArtistsPage';
+import TracksPage from './pages/TracksPage';
 
-// export default App;
+/**
+ * Основной компонент приложения с маршрутизацией.
+ */
+const App: React.FC = () => (
+  <Router
+    future={{
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    }}
+  >
+    <Header />
+    <main>
+      <Routes>
+        <Route path="/" element={<div></div>} />
+        <Route path="/artists" element={<ArtistsPage />} />
+        <Route path="/tracks" element={<TracksPage />} />
+      </Routes>
+    </main>
+  </Router>
+);
+
+export default App;
